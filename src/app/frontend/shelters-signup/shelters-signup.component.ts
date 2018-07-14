cfadc7ce908a1e8b83fa34459111e344dfa5ef89
Malcolm Childs <michilds0@gmail.com>
2018-07-14 17:03:00 -0400

    Updated font sizes and shelters home layout

--- src/app/frontend/shelters-signup/shelters-signup.component.ts
+++ src/app/frontend/shelters-signup/shelters-signup.component.ts
@@ -1,53 +1,52 @@
-
 import { Component, OnInit } from '@angular/core';
 import { ShelterService } from '@appCore/services/shelter.service';
 import { ActivatedRoute } from '@angular/router';
 import { filter } from 'rxjs/operators';
-import {MatSnackBar} from '@angular/material';
+import { MatSnackBar } from '@angular/material';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-    selector: 'app-shelters-signup',
-    templateUrl: 'shelters-signup.component.html',
-    styleUrls: ['./shelters-signup.component.css']
+  selector: 'app-shelters-signup',
+  templateUrl: 'shelters-signup.component.html',
+  styleUrls: ['./shelters-signup.component.css']
 })
 
 export class SheltersSignupComponent implements OnInit {
-  signUpForm: FormGroup; 
-  
-public shelterName: string;
-public shelterAddressNumber: number;
-public shelterCity: string;
-public shelterState: number;
-public shelterTel: number;
-public shelterCounty: string;
-public shelterEmail: string;
+  signUpForm: FormGroup;
 
-    constructor(
-        private _activated: ActivatedRoute,
-        private _shelterService: ShelterService,
-        public snackBar: MatSnackBar,
-        private fb: FormBuilder
-    ) { }
+  public shelterName: string;
+  public shelterAddressNumber: number;
+  public shelterCity: string;
+  public shelterState: number;
+  public shelterTel: number;
+  public shelterCounty: string;
+  public shelterEmail: string;
 
-    ngOnInit() {
-      console.log('YO');
-      this.createForm()
-    }
-    createForm() {
-      this.signUpForm = this.fb.group({
-        username: ['', Validators.required ],
-        password:['', Validators.required ],
-        street: ['', Validators.required ],
-        city: ['', Validators.required ],
-        state: ['', Validators.required ],
-        zipCode: ['', Validators.required ],
-        phone: ['', Validators.required ],
-        county: ['', Validators.required ],
-        email: ['', Validators.required ],
-      });
-    }
-  public  save(form) {
+  constructor(
+    private _activated: ActivatedRoute,
+    private _shelterService: ShelterService,
+    public snackBar: MatSnackBar,
+    private fb: FormBuilder
+  ) { }
+
+  ngOnInit() {
+    console.log('YO');
+    this.createForm()
+  }
+  createForm() {
+    this.signUpForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+      street: ['', Validators.required],
+      city: ['', Validators.required],
+      state: ['', Validators.required],
+      zipCode: ['', Validators.required],
+      phone: ['', Validators.required],
+      county: ['', Validators.required],
+      email: ['', Validators.required],
+    });
+  }
+  public save(form) {
     console.log(form.value)
     const data = {
       shelter_name: form.value.name,
@@ -59,16 +58,16 @@ public shelterEmail: string;
       shelter_email: form.value.email,
       shelter_zipCode: form.value.zipCode
     };
-console.log(data);
+    console.log(data);
 
-this._shelterService.signupShelters(data)
-  .subscribe(result => {
-    console.log(result);
-  });
-  this.snackBar.open('Saved ', 'close', {
-    duration: 2000,
-  });
-}
+    // this._shelterService.signupShelters(data)
+    //   .subscribe(result => {
+    //     console.log(result);
+    //   });
+    //   this.snackBar.open('Saved ', 'close', {
+    //     duration: 2000,
+    //   });
+  }
 
 
 }
