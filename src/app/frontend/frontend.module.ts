206c411b710f57cada7a2a350afaeb9fe72bb73b
Malcolm Childs <michilds0@gmail.com>
2018-08-18 12:47:38 -0400

    minor updates

--- src/app/frontend/frontend.module.ts
+++ src/app/frontend/frontend.module.ts
@@ -6,21 +6,18 @@ import { FrontendRoutingModule } from '@appFrontend/frontend-routing.module';
 import { HomeComponent } from '@appFrontend/home/home.component';
 import { ShelterItemComponent } from '@appFrontend/shelter-item/shelter-item.component';
 import { SheltersComponent } from '@appFrontend/shelters/shelters.component';
-<<<<<<< HEAD
 import { FormsModule } from '@angular/forms';
 import { FiltersComponent } from '@appFrontend/filters/filters.component';
 
-=======
 
-import {MatInputModule} from '@angular/material/input';
-import {MatTableModule} from '@angular/material/table';
-import {FormsModule} from '@angular/forms';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatCardModule} from '@angular/material/card';
-import {MatSelectModule} from '@angular/material/select';
-import {MatButtonModule} from '@angular/material/button';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
->>>>>>> upstream/master
+// import {MatInputModule} from '@angular/material/input';
+// import {MatTableModule} from '@angular/material/table';
+// import {FormsModule} from '@angular/forms';
+// import {MatFormFieldModule} from '@angular/material/form-field';
+// import {MatCardModule} from '@angular/material/card';
+// import {MatSelectModule} from '@angular/material/select';
+// import {MatButtonModule} from '@angular/material/button';
+// import {MatSnackBarModule} from '@angular/material/snack-bar';
 
 
 @NgModule({
@@ -38,14 +35,11 @@ import {MatSnackBarModule} from '@angular/material/snack-bar';
     ShelterItemComponent,
     HomeComponent,
     SheltersComponent,
-<<<<<<< HEAD
     FiltersComponent
   ],
-  entryComponents: [
-    FiltersComponent
-=======
-    
->>>>>>> upstream/master
-  ]
+  // entryComponents: [
+  //   FiltersComponent
+
+  // ]
 })
 export class FrontendModule { }
